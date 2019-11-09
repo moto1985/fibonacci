59fff2f1d72b09db9cf50a5d725b8a399593fd03
moto1985 <moto19851218@gmail.com>
2019-11-09 22:49:45 +0900

    アルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict';
+const fibMap = new Map();
+fibMap.set(0,0);
+fibMap.set(1,1);
+
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if (fibMap.has(n)) {
+    return fibMap.get(n);
   }
-  return fib(n - 1) + fib(n - 2);
+  const value = fib(n - 1) + fib(n - 2);
+  fibMap.set(n,value);
+  return value;
 }
-const length = 40;
-for (let i = 1; i <= length; i++) {
+const length = 100;
+for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
